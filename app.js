57f905dc977f2d90e90ca2675e390d9509353aa7
perrinbm97 <perrinbm08@gmail.com>
2026-02-25 12:18:45 -0500

    Q7-11

--- app.js
+++ app.js
@@ -53,15 +53,87 @@ console.log(progressiveSum(200));
  * the time in mm:ss format */
 
 function calcTime(sec) {
-    let timeMin = Math.floor(sec / 60);
-    let timeSec = sec % 60;
-    if (timeMin.toString().length === 1) {
-        timeMin = "0" + timeMin;
-    }
-    if (timeSec < 10) {
-        timeSec = "0" + timeSec;
-    }
-    return timeMin + ':' + timeSec;
+  let timeMin = Math.floor(sec / 60);
+  let timeSec = sec % 60;
+  if (timeMin.toString().length === 1) {
+    timeMin = "0" + timeMin;
+  }
+  if (timeSec < 10) {
+    timeSec = "0" + timeSec;
+  }
+  return timeMin + ":" + timeSec;
 }
 
 console.log(calcTime(125));
+
+/** 7.) Given an array of numbers, return
+ * the largest */
+
+function getMax(arr) {
+  let maxNum = arr[0];
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i] > maxNum) {
+      maxNum = arr[i];
+    }
+  }
+  return maxNum;
+}
+
+console.log(getMax([5, 100, 25]));
+
+/** 8.) Given a string, return the
+ * reversed version */
+
+function reverseString(str) {
+  // let reverse = '';
+  // for (i=1; i<=str.length; i++) {
+  //   reverse += str[str.length-i];
+  // }
+  // return reverse;
+  return str.split("").reverse("").join("");
+}
+
+console.log(reverseString("Incredible"));
+
+/** 9.) Given an array, return the
+ * same length array with all zeros */
+
+function oopsAllZeros(arr) {
+  // let newArr = [];
+  // for (i = 0; i < arr.length; i++) {
+  //   newArr[i] = 0;
+  // }
+  // return newArr;
+
+  // return new Array(arr.length).fill(0);
+
+  return arr.map((elem) => 0);
+}
+
+console.log(oopsAllZeros(["String", 100, `Zero`, 25, 0]));
+
+/** 10.) Given array, if it is an apple
+ * remove it from the array */
+
+function filterApples(arr) {
+  // let nonApples = [];
+  // for (i = 0; i < arr.length; i++) {
+  //   if (arr[i] !== "Apple") {
+  //     nonApples.push(arr[i]);
+  //   }
+  // }
+  // return nonApples;
+
+  return arr.filter((elem) => elem !== "Apple");
+}
+
+console.log(filterApples(["Banana", "Apple", "Orange", "Apple"]));
+
+/** 11.) Given array, filter out falsy values
+ * and only return truthy values */
+
+function removeFalsy (arr) {
+  return arr.filter(elem => !!elem)
+}
+
+console.log(removeFalsy(['', 'True', 0, null]));
